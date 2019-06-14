846bf847555372dbed83d4d2780160ac687ff438
Карпов Владислав Владимирович <Karpov.V.Vladim@sberbank.ru>
2019-06-14 15:53:12 +0300

    origin

--- src/main/resources/collections.java
+++ src/main/resources/collections.java
@@ -24,13 +24,35 @@ public interface Collection<E> extends Iterable<E> {
     default Stream<E> parallelStream();
 }
 public interface Queue<E> extends java.util.Collection<E> {
-    boolean add(E e);
-    boolean offer(E e);
-    E remove();
-    E poll();
-    E element();
-    E peek();
-}
+    boolean add(E e);           //Inserts the specified element into this queue
+    boolean offer(E e);         //Inserts the specified element into this queue. preferable to {@link #add}
+    E remove();                 //Retrieves and removes the head of this queue. NoSuchElementException
+    E poll();                   //Retrieves and removes the head of this queue
+    E element();                //Retrieves, but does not remove, the head of this queue.  NoSuchElementException
+    E peek();                   //Retrieves, but does not remove, the head of this queue
+}   //PriorityQueue
+public interface Deque<E> extends java.util.Queue<E> {
+    void addFirst(E e);         //Inserts the specified element at the front of this deque
+    void addLast(E e);          //Inserts the specified element at the end of this deque
+    boolean offerFirst(E e);    //Inserts the specified element at the front
+    boolean offerLast(E e);     //Inserts the specified element at the end of this deque
+    E removeFirst();            //Retrieves and removes the first element of this deque. NoSuchElementException
+    E removeLast();             //Retrieves and removes the last element of this deque. NoSuchElementException
+    E pollFirst();              //Retrieves and removes the first element of this deque
+    E pollLast();               //Retrieves and removes the last element of this deque
+    E getFirst();               //Retrieves, but does not remove, the first element of this deque.NoSuchElementException
+    E getLast();                //Retrieves, but does not remove, the last element of this deque.NoSuchElementException
+    E peekFirst();              //Retrieves, but does not remove, the first element of this deque,
+    E peekLast();               //Retrieves, but does not remove, the last element of this deque,
+    boolean removeFirstOccurrence(Object o); //Removes the first occurrence of the specified element from this deque.
+    boolean removeLastOccurrence(Object o); //Removes the last occurrence of the specified element from this deque.
+    // *** Queue methods ***
+    //add offer remove poll element peek
+    // *** Stack methods ***
+    void push(E e); //Pushes an element onto the stack represented by this deque (in other words, at the head)
+    E pop(); //Pops an element from the stack represented by this deque. (removes and returns the first element)
+    // *** Collection methods ***
+}   //ArrayDeque, LinkedList
 public interface List<E> extends java.util.Collection<E> {
     // Query Operations
     int size();
@@ -68,7 +90,7 @@ public interface List<E> extends java.util.Collection<E> {
     // View
     java.util.List<E> subList(int fromIndex, int toIndex);
     default Spliterator<E> spliterator();
-}
+}   //ArrayList, LinkedList, Vector, Stack
 public interface Set<E> extends java.util.Collection<E> {
     // Query Operations
     int size();
@@ -90,7 +112,7 @@ public interface Set<E> extends java.util.Collection<E> {
     boolean equals(Object o);
     int hashCode();
     default Spliterator<E> spliterator();
-}
+}   //HashSet, LinkedHashSet
 public interface SortedSet<E> extends java.util.Set<E> {
     Comparator<? super E> comparator();
     java.util.SortedSet<E> subSet(E fromElement, E toElement);
@@ -101,12 +123,12 @@ public interface SortedSet<E> extends java.util.Set<E> {
     default Spliterator<E> spliterator();
 }
 public interface NavigableSet<E> extends java.util.SortedSet<E> {
-    E lower(E e);
-    E floor(E e);
-    E ceiling(E e);
-    E higher(E e);
-    E pollFirst();
-    E pollLast();
+    E lower(E e); //ниже greatest element in this set strictly less than e
+    E floor(E e); //пол  greatest element in this set less than or equal to e
+    E ceiling(E e); //потолок least element in this set greater than or equal to e
+    E higher(E e); //выше least element in this set greater than or equal to e
+    E pollFirst();  //Retrieves and removes the first (lowest) element
+    E pollLast();   //Retrieves and removes the last (highest) element
     Iterator<E> iterator();
     java.util.NavigableSet<E> descendingSet();
     Iterator<E> descendingIterator();
@@ -116,7 +138,7 @@ public interface NavigableSet<E> extends java.util.SortedSet<E> {
     java.util.SortedSet<E> subSet(E fromElement, E toElement);
     java.util.SortedSet<E> headSet(E toElement);
     java.util.SortedSet<E> tailSet(E fromElement);
-}
+}   //TreeSet
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 public interface Map<K,V> {
     // Query Operations
@@ -151,7 +173,7 @@ public interface Map<K,V> {
     default V computeIfPresent(K key, BiFunction<? super K, ? super V, ? extends V> remappingFunction);
     default V compute(K key, BiFunction<? super K, ? super V, ? extends V> remappingFunction);
     default V merge(K key, V value, BiFunction<? super V, ? super V, ? extends V> remappingFunction);
-}
+}   //HashMap, Hashtable, ConcurrentHashMap, LinkedHashMap,
 public interface SortedMap<K,V> extends java.util.Map<K,V> {
     Comparator<? super K> comparator();
     java.util.SortedMap<K,V> subMap(K fromKey, K toKey);
@@ -185,4 +207,4 @@ public interface NavigableMap<K,V> extends java.util.SortedMap<K,V> {
     java.util.SortedMap<K,V> subMap(K fromKey, K toKey);
     java.util.SortedMap<K,V> headMap(K toKey);
     java.util.SortedMap<K,V> tailMap(K fromKey);
-}
+} //TreeMap
