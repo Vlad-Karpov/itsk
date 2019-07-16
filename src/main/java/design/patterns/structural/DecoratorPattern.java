6863f29f8d9779b0fee0e578f216393afa279b77
Карпов Владислав Владимирович <Karpov.V.Vladim@sberbank.ru>
2019-07-16 17:26:49 +0300

    Patterns

---
+++ src/main/java/design/patterns/structural/DecoratorPattern.java
@@ -0,0 +1,100 @@
+package design.patterns.structural;
+
+/**
+ * Декоратор (англ. Decorator) — структурный шаблон проектирования, предназначенный для динамического подключения
+ * дополнительного поведения к объекту. Шаблон Декоратор предоставляет гибкую альтернативу практике создания подклассов
+ * с целью расширения функциональности.
+ */
+public class DecoratorPattern {
+
+    interface InterfaceComponent {
+        void doOperation();
+    }
+
+    static class MainComponent implements InterfaceComponent {
+
+        @Override
+        public void doOperation() {
+            System.out.print("World!");
+        }
+    }
+
+    abstract static class Decorator implements InterfaceComponent {
+        protected InterfaceComponent component;
+
+        public Decorator (InterfaceComponent c) {
+            component = c;
+        }
+
+        @Override
+        public void doOperation() {
+            component.doOperation();
+        }
+
+        public void newOperation() {
+            System.out.println("Do Nothing");
+        }
+    }
+
+    static class DecoratorSpace extends Decorator {
+
+        public DecoratorSpace(InterfaceComponent c) {
+            super(c);
+        }
+
+        @Override
+        public void doOperation() {
+            System.out.print(" ");
+            super.doOperation();
+        }
+
+        @Override
+        public void newOperation() {
+            System.out.println("New space operation");
+        }
+    }
+
+    static class DecoratorComma extends Decorator {
+
+        public DecoratorComma(InterfaceComponent c) {
+            super(c);
+        }
+
+        @Override
+        public void doOperation() {
+            System.out.print(",");
+            super.doOperation();
+        }
+
+        @Override
+        public void newOperation() {
+            System.out.println("New comma operation");
+        }
+    }
+
+    static class DecoratorHello extends Decorator {
+
+        public DecoratorHello(InterfaceComponent c) {
+            super(c);
+        }
+
+        @Override
+        public void doOperation() {
+            System.out.print("Hello");
+            super.doOperation();
+        }
+
+        @Override
+        public void newOperation() {
+            System.out.println("New hello operation");
+        }
+    }
+
+    public static void main (String... s) {
+        Decorator c = new DecoratorHello(new DecoratorComma(new DecoratorSpace(new MainComponent())));
+        c.doOperation(); // Результат выполнения программы "Hello, World!"
+        System.out.println("");
+        c.newOperation(); // New hello operation
+    }
+
+}
