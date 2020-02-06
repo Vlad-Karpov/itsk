d6c79d8611efd68b99899e15d425b7f47120be19
Карпов Владислав Владимирович <karpovv@dev.vtb>
2020-02-06 12:00:44 +0300

    heapSort

--- src/main/java/yandex/sort/inner/Heap.java
+++ src/main/java/yandex/sort/inner/Heap.java
@@ -12,8 +12,8 @@ public class Heap {
         return innerHeapSort(list, sortContextObject);
     }
 
-    private static <T extends Comparable<? super T>> List<T> innerHeapSort(List<T> list, SortContextInterface sortContextObject) {
-        int cld = 0;
+    private static <T extends Comparable<? super T>> List<T> innerHeapSort(
+            List<T> list, SortContextInterface sortContextObject) {
         int c = 0;
         int d = 0;  //direction: 0 - come from parent, 1 - from left, 2 - from right
         int s = list.size();
@@ -38,20 +38,11 @@ public class Heap {
                     break;
                 }
                 case 2: {
-                    if (c < s) {
-                        cld = (c << 1) + 1;
-                        if (cld < s) {
-                            cmpAndSwap(list, cld, c, sortContextObject);
-                        }
-                        cld = (c << 1) + 2;
-                        if (cld < s) {
-                            cmpAndSwap(list, cld, c, sortContextObject);
-                        }
-                    }
-                    if ((c & 1) == 0) {         //even - from right
+                    sift(list, c, s, sortContextObject);
+                    if ((c & 1) == 0) {     //even - from right
                         c = c - 2 >> 1;
                         d = 2;
-                    } else {            //odd - from left
+                    } else {                //odd - from left
                         c = c - 1 >> 1;
                         d = 1;
                     }
@@ -59,21 +50,75 @@ public class Heap {
                 }
             }
         }
+        while (s > 0) {
+            sortContextObject.incSwapCount();
+            T sV = list.get(s - 1);
+            T hV = list.get(0);
+            list.set(0, sV);
+            list.set(s - 1, hV);
+            s--;
+            sift(list, 0, s, sortContextObject);
+        }
         return list;
     }
 
-    private static <T extends Comparable<? super T>> void sift(List<T> list, int c, SortContextInterface sortContextObject) {
-
-    }
-
-    private static <T extends Comparable<? super T>> void cmpAndSwap(List<T> list, int cld, int c, SortContextInterface sortContextObject) {
-        T cldV = list.get(cld);
-        T cV = list.get(c);
-        sortContextObject.incCmpCount();
-        if (cldV.compareTo(cV) > 0) {
-            list.set(cld, cV);
-            list.set(c, cldV);
-            sortContextObject.incSwapCount();
+    private static <T extends Comparable<? super T>> void sift(
+            List<T> list, int c, int s,
+            SortContextInterface sortContextObject) {
+        while (c < s) {
+            int l = (c << 1) + 1;
+            int r = (c << 1) + 2;
+            T lV = null;
+            T rV = null;
+            boolean ok = false;
+            if (l < s) {
+                lV = list.get(l);
+                ok = true;
+            }
+            if (r < s) {
+                rV = list.get(r);
+                ok = true;
+            }
+            if (ok) {
+                T cV = list.get(c);
+                int ch = -1;
+                T chV = null;
+                sortContextObject.incCmpCount();
+                int cmp;
+                if (lV == null) {
+                    cmp = -1;
+                } else {
+                    if (rV == null) {
+                        cmp = 1;
+                    } else {
+                        cmp = lV.compareTo(rV);
+                    }
+                }
+                if (cmp > 0) {
+                    sortContextObject.incCmpCount();
+                    if (cV.compareTo(lV) < 0) {
+                        ch = l;
+                        chV = lV;
+                    }
+                }
+                if (cmp < 0) {
+                    sortContextObject.incCmpCount();
+                    if (cV.compareTo(rV) < 0) {
+                        ch = r;
+                        chV = rV;
+                    }
+                }
+                if (ch > 0) {
+                    sortContextObject.incSwapCount();
+                    list.set(ch, cV);
+                    list.set(c, chV);
+                    c = ch;
+                } else {
+                    break;
+                }
+            } else {
+                break;
+            }
         }
     }
 
--- src/test/java/yandex/sort/inner/HeapTest.java
+++ src/test/java/yandex/sort/inner/HeapTest.java
@@ -4,6 +4,7 @@ import org.junit.Test;
 import yandex.sort.SortContext;
 
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
 
 public class HeapTest extends SortGeneralTest {
@@ -19,8 +20,75 @@ public class HeapTest extends SortGeneralTest {
         list.add(5L);
         list.add(6L);
         list.add(7L);
+        list.add(8L);
         Heap.heapSort(list, sc);
-        Heap.heapPrint(list, System.out::println);
+        Heap.heapPrint(list, l -> {
+            System.out.print(l + ", ");
+        });
+        System.out.println("");
+        list.forEach(e -> System.out.print(e + ", "));
+        System.out.println("");
     }
+
+    @Test
+    public void test2() {
+        SortContext sc = new SortContext();
+        List<Long> list = new ArrayList<Long>();
+        Heap.heapSort(list, sc);
+        checkList(list);
+        list.add(10L);
+        Heap.heapSort(list, sc);
+        checkList(list);
+        list.add(20L);
+        Heap.heapSort(list, sc);
+        checkList(list);
+        list.add(15L);
+        Heap.heapSort(list, sc);
+        checkList(list);
+
+        list.clear();
+        Long rnd;
+        for (int i = 0; i < 10000000; i++) {
+            rnd = Math.round(Math.random() * 100000L);
+            list.add(rnd);
+        }
+
+        Calendar cBegin = Calendar.getInstance();
+        Heap.heapSort(list, sc);
+        Calendar cEnd = Calendar.getInstance();
+        System.out.println("time = " + (cEnd.getTimeInMillis() - cBegin.getTimeInMillis()));
+        System.out.println(sc);
+
+        checkList(list);
+    }
+
+    @Test
+    public void test3() {
+        SortContext sc = new SortContext();
+        List<Long> list = new ArrayList<Long>();
+        list.add(9L);
+        list.add(5L);
+        list.add(8L);
+        list.add(5L);
+        list.add(17L);
+        list.add(5L);
+        list.add(6L);
+        list.add(5L);
+        list.add(5L);
+        list.add(5L);
+        list.add(4L);
+        list.add(13L);
+        list.add(5L);
+        list.add(2L);
+        list.add(5L);
+        list.add(1L);
+        list.add(5L);
+        list.add(5L);
+        list.add(5L);
+        Heap.heapSort(list, sc);
+        list.forEach(e -> System.out.print(e + ", "));
+        checkList(list);
+    }
+
 
 }
